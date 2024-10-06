36819b8becd29fb5099326928e48b81ca23865fd
Rony <raug01@gmail.com>
2024-10-05 21:57:19 -0400

    add experience

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import styles from'./App.module.css'
 import { Navbar } from './components/Navbar/Navbar';
 import {Hero} from './components/Hero/Hero'
 import { About } from './components/About/About';
+import { Experience } from './components/Experience/experience';
 
 function App() {
    return <div className={styles.App}>
       <Navbar />
       <Hero />
       <About />
+      <Experience />
+
 
    </div>;
   
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,44 +7,11 @@ export const About = () => {
         <section className={styles.container} id ="about">
             <h2 className={styles.title}>About</h2>
             <div><img src={getImageUrl("about/about.png")} className={styles.aboutImage} alt="About Me" /></div>
-            <ul className={styles.aboutItems}>
-                <li className={styles.aboutItem}>
-                    <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon"/>
-                    <div className={styles.aboutItemText}>
-                        <h3>FrontEnd Developer</h3>
-                        <p>During my studies, I learned various programming languages and frameworks, such as:</p>
-                    </div>
-                    <li >
-                    <br/>
-                        <img src={getImageUrl("about/frontend/html.png")} className={styles.responsiveImage} alt="Html icon"/>
-                        <img src={getImageUrl("about/frontend/css.png")} className={styles.responsiveImage} alt="CSS icon" />
-                        <img src={getImageUrl("about/frontend/php.png")} className={styles.responsiveImage} alt="PHP icon" />
-                        <img src={getImageUrl("about/frontend/javascript.png")} className={styles.responsiveImage} alt="JS icon" />
-                        <img src={getImageUrl("about/frontend/react.png")} className={styles.responsiveImage} alt="React icon" />
-                        <img src={getImageUrl("about/frontend/bootstrap.png")} className={styles.responsiveImage} alt="Bootstrap icon" />
-                    </li>
-                    <br/><br/>
             
-                </li>
-                <li className={styles.aboutItem}>
-                    <img src={getImageUrl("about/serverIcon.png")} alt="Server icon"/>
                     <div className={styles.aboutItemText}>
-                        <h3>BackEnd Developer</h3>
-                        <p>I focused on learning different back-end languages and frameworks during my studies, including:</p>
-                    </div>
-                        <li >
-                            <br/>
-                            <img src={getImageUrl("about/backend/asp.png")} className={styles.responsiveImage} alt="Asp icon" />
-                            <img src={getImageUrl("about/backend/csharp.png")} className={styles.responsiveImage} alt="Csharp icon" />
-                            <img src={getImageUrl("about/backend/java.png")} className={styles.responsiveImage} alt="Java icon" />
-                            <img src={getImageUrl("about/backend/python.png")} className={styles.responsiveImage} alt="Python icon" />
-                            <img src={getImageUrl("about/backend/sql.png")} className={styles.responsiveImage} alt="SQL icon" />
-                            <img src={getImageUrl("about/backend/spring-boot.png")} className={styles.responsiveImage} alt="Spring Boot icon" />
-                            <img src={getImageUrl("about/backend/swift.png")} className={styles.responsiveImage} alt="Swift icon" />
-                            <img src={getImageUrl("about/backend/android.png")} className={styles.responsiveImage} alt="Android icon" />
-                        </li>                  
-                </li>
-            </ul>
+                       
+                        <h3 className={styles.aboutItems}>I love taking on challenges and finding ways to make work easier and more efficient for myself and my team. Now that I’ve finished my Programmer-Analyst program at LaSalle College, I’m focused on improving my skills and exploring new opportunities for growth in the IT field.</h3>
+                  </div>
             
         </section>
     )
