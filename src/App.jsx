05d366ce698be5efab063cb114916e7d24134741
Rony <raug01@gmail.com>
2024-10-06 10:51:51 -0400

    add portfolio layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import { Navbar } from './components/Navbar/Navbar';
 import {Hero} from './components/Hero/Hero'
 import { About } from './components/About/About';
 import { Experience } from './components/Experience/experience';
-
+import { Projects } from './components/Projects/Projects';
 function App() {
    return <div className={styles.App}>
       <Navbar />
       <Hero />
       <About />
       <Experience />
+      <Projects />
 
 
    </div>;
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,9 +8,9 @@ export const About = () => {
             <h2 className={styles.title}>About</h2>
             <div><img src={getImageUrl("about/about.png")} className={styles.aboutImage} alt="About Me" /></div>
             
-                    <div className={styles.aboutItemText}>
+                    <div className={styles.aboutItem}>
                        
-                        <h3 className={styles.aboutItems}>I love taking on challenges and finding ways to make work easier and more efficient for myself and my team. Now that I’ve finished my Programmer-Analyst program at LaSalle College, I’m focused on improving my skills and exploring new opportunities for growth in the IT field.</h3>
+                        <p className={styles.aboutItems}>I love taking on challenges and finding ways to make work easier and more efficient for myself and my team. Now that I’ve finished my Programmer-Analyst program at LaSalle College, I’m focused on improving my skills and exploring new opportunities for growth in the IT field.</p>
                   </div>
             
         </section>
