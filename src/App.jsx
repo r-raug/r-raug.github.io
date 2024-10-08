63d33eb89c0a5b345142f76549cc6510175acb8e
Rony <raug01@gmail.com>
2024-10-07 21:17:00 -0400

    update projects

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import { Experience } from './components/Experience/Experience';
 import { Projects } from './components/Projects/Projects';
 import { Contact } from './components/Contact/Contact';
 import { Education } from './components/Education/Education';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
    return <div className={styles.App}>
       <Navbar />
--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -13,9 +13,8 @@ export const ProjectCard = ({ project: {title, imageSrc, skills, demo, source, d
                     <li key={id} className={styles.skill}>{skill}</li>
                 ))}
             </ul>
-            <div className={styles.links}>
-                <a href={demo} className={styles.link}>Demo</a>
-                <a href={source} className={styles.link}>Source</a>
+            <div className={styles.links}>                
+                <a href={source} className="btn btn-outline-primary" target="_blank">Source</a>
             </div>
         </div>
     );
